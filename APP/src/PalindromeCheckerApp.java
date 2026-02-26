5188c18760b3151aed7e73420fc08a5a3007d2f3
swasti.ranjan <swasti.ranjan21@gmail.com>
2026-02-26 14:16:05 +0530

    uc1: application entry and welocme message

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("welcome to palindrome checker management system");
+        System.out.println("version 1.0");
+        System.out.println("system initialised successfully");
+    }
+}
