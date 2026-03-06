ac1163c90e6a50edbda2278fdbb563f665ab631c
swastiranjan <swasti.ranjan21@gmail.com>
2026-03-06 14:16:50 +0530

    uc9: recursive palindrome checker

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,68 +1,28 @@
-class Node {
-    char data;
-    Node next;
+public class PalindromeCheckerApp {
 
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-public class PalindromeCheckerApp{
-
-    private static Node buildLinkedList(String input) {
-        input = input.replaceAll("\\s+", "").toLowerCase();
-        Node head = null, tail = null;
-
-        for (char ch : input.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
+    // Recursive method to check palindrome
+    public static boolean isPalindromeRecursive(String s, int start, int end) {
+        // Base case: if start crosses end, it's a palindrome
+        if (start >= end) {
+            return true;
         }
-        return head;
-    }
 
-    private static Node reverse(Node head) {
-        Node prev = null, current = head, next;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+        // If mismatch found
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
         }
-        return prev;
+
+        // Recursive call: move inward
+        return isPalindromeRecursive(s, start + 1, end - 1);
     }
 
+    // Public method to call recursion
     public static boolean isPalindrome(String input) {
-        Node head = buildLinkedList(input);
-
-        if (head == null || head.next == null) return true;
-
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-
-        Node firstHalf = head;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+        input = input.replaceAll("\\s+", "").toLowerCase();
+        return isPalindromeRecursive(input, 0, input.length() - 1);
     }
 
+    // Driver code
     public static void main(String[] args) {
         String test1 = "madam";
         String test2 = "racecar";
