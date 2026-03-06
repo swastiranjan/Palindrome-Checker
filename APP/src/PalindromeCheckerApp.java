10d76e77ea447cb99f208752b636ca7ae0449311
swastiranjan <swasti.ranjan21@gmail.com>
2026-03-06 14:05:39 +0530

    uc8: linkedlist based palindrome checker app

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,30 +1,68 @@
-import java.util.Deque;
-import java.util.LinkedList;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp{
 
-    public static boolean isPalindrome(String input) {
+    private static Node buildLinkedList(String input) {
         input = input.replaceAll("\\s+", "").toLowerCase();
-
-        Deque<Character> deque = new LinkedList<>();
+        Node head = null, tail = null;
 
         for (char ch : input.toCharArray()) {
-            deque.addLast(ch);
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
+    private static Node reverse(Node head) {
+        Node prev = null, current = head, next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(String input) {
+        Node head = buildLinkedList(input);
+
+        if (head == null || head.next == null) return true;
+
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
+        Node secondHalf = reverse(slow);
 
-            if (front != rear) {
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 return false;
             }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
 
         return true;
     }
 
-    // Driver code
     public static void main(String[] args) {
         String test1 = "madam";
         String test2 = "racecar";
